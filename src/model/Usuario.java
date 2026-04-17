86194ec15fcbcefc04707d95075b8a17515759e6
sabrynavn <Sabrynanoronhav@gmail.com>
2026-04-16 23:02:55 -0300

    feat(model): adição de getters e método exibirDados na classe Usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -17,4 +17,31 @@ public class Usuario {
         this.telefone = telefone;
         this.email = email;
     }
+
+    // Getters (acesso aos dados)
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Exibe os dados básicos do usuário no terminal
+     */
+    public void exibirDados() {
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Email: " + email);
+    }
 }
